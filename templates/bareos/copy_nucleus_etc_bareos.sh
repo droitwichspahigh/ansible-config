59fbfbd127cf8b28314dcdd2af6becb3ffe1b785
Chris Rees <crees@FreeBSD.org>
2026-08-06 20:45:18 +0100

    Fix

--- templates/bareos/copy_nucleus_etc_bareos.sh
+++ templates/bareos/copy_nucleus_etc_bareos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-printf '%s\n%s\n%s\n%s' 'restore client=dshs-nucleus restoreclient=dshs-backup-j1 add_prefix=/usr/local/etc/bareos/nucleus-etc strip_prefix=/usr/local/etc select current' 'mark /usr/local/etc/bareos' 'done' 'yes'| /usr/local/sbin/bconsole
+printf '%s\n%s\n%s\n%s' 'restore client=dshs-nucleus restoreclient=dshs-backup-j1 fileset=UNIX-zfs-vss add_prefix=/usr/local/etc/bareos/nucleus-etc strip_prefix=/usr/local/etc select current' 'mark /usr/local/etc/bareos' 'done' 'yes'| /usr/local/sbin/bconsole
